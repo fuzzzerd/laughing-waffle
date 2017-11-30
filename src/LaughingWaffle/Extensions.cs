16dc383e09f8ac683ac3c2c34d7744404a192373
Nate Bross <nate@brosstribe.com>
2017-11-29 23:48:06 -0600

    fleshed out extension, updated comments, added watcher.

--- src/LaughingWaffle/Extensions.cs
+++ src/LaughingWaffle/Extensions.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Text;
 using System.Data.SqlClient;
+using System.Linq;
 
 namespace LaughingWaffle
 {
@@ -11,11 +12,13 @@ namespace LaughingWaffle
     {
         public static bool Upsert<TEntity>(
             this IDbConnection conn,
-            IEnumerable<TEntity> entities)
+            IEnumerable<TEntity> entities,
+            IUpsertOptions<TEntity> options)
         {
             // setup 
             var sqlGenerator = new TSqlGenerator<TEntity>();
             var createScript = sqlGenerator.CreateTable();
+            var mergeScript = sqlGenerator.Merge(options);
 
             // execute
             var createTableCmd = conn.CreateCommand();
@@ -25,22 +28,32 @@ namespace LaughingWaffle
 
             // ==> bulk insert to temp table
             using (var copy = new SqlBulkCopy((SqlConnection)conn))
+            using(var reader = FastMember.ObjectReader.Create(entities, sqlGenerator.GetProperties().ToArray()))
             {
                 // setup mapping
-
-                // run the merge
+                foreach (var prop in sqlGenerator.GetProperties())
+                {
+                    // force one<=>one mapping
+                    copy.ColumnMappings.Add(prop, prop);
+                }
+                // write data from reader to server
+                copy.WriteToServer(reader);
+                // run the bulk insert to temp table
             }
             // <== end bulk insert to temp table
 
+            // ==> merge upsert
             using (var trans = conn.BeginTransaction())
             {
                 // ==> merge temp table to target table
-                //var mergeTempAndRealCmd = conn.CreateCommand();
-                //mergeTempAndRealCmd.CommandType = CommandType.Text;
-                //mergeTempAndRealCmd.CommandText = mergeTempAndRealCmd;
-                //mergeTempAndRealCmd.ExecuteNonQuery();
+                var mergeTempAndRealCmd = conn.CreateCommand();
+                mergeTempAndRealCmd.CommandType = CommandType.Text;
+                mergeTempAndRealCmd.CommandText = mergeScript;
+                mergeTempAndRealCmd.ExecuteNonQuery();
                 // <== end merge temp table to target table
+                trans.Commit();
             }
+            // <== end merge upsert
 
             // cleanup
 
--- src/LaughingWaffle/IUpsertOptions.cs
+++ src/LaughingWaffle/IUpsertOptions.cs
@@ -6,15 +6,53 @@ namespace LaughingWaffle
 {
     public interface IUpsertOptions<TType>
     {
+        /// <summary>
+        /// Set the name of the target table for this upsert. This method defaults the schema to "dbo".
+        /// </summary>
+        /// <param name="table">The name of the target table. </param>
+        /// <returns>Instance of the parent class for method chaining.</returns>
         UpsertOptions<TType> SetTargetTable(string table);
+        
+        /// <summary>
+        /// Set the name of the target table and schema for this upsert.
+        /// </summary>
+        /// <param name="schema">The schema name to use.</param>
+        /// <param name="table">The name of the target table.</param>
+        /// <returns>Instance of the parent class for method chaining.</returns>
         UpsertOptions<TType> SetTargetTableWithSchema(string schema, string table);
+        
+        /// <summary>
+        /// Add a column to the MAP list. The columns that are updated or inserted into.
+        /// </summary>
+        /// <param name="func">The name of the column.</param>
+        /// <returns>Instance of the parent class for method chaining.</returns>
         UpsertOptions<TType> AddMapColumn(Expression<Func<TType, object>> func);
+        
+        /// <summary>
+        /// Add a column to the MATCH list. The columns used to determine equality of rows (for update vs. insert: Upsert).
+        /// </summary>
+        /// <param name="func">The name of the column.</param>
+        /// <returns>Instance of the parent class for method chaining.</returns>
         UpsertOptions<TType> AddMatchColumn(Expression<Func<TType, object>> func);
 
+        /// <summary>
+        /// The columns to match on.
+        /// </summary>
         IEnumerable<string> MatchColumns { get; }
+        
+        /// <summary>
+        /// The columns to map (that is update, or insert data from/to)
+        /// </summary>
         IEnumerable<string> MapColumns { get; }
 
+        /// <summary>
+        /// Name of the target Table for the Upsert.
+        /// </summary>
         string TargetTableName { get; }
+        
+        /// <summary>
+        /// Name of the target Schema for the Upsert.
+        /// </summary>
         string TargetTableSchema { get; }
     }
 }
